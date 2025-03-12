91c7213cdbadf86c5a09a5c48b0f934229e06f35
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-03-12 12:13:34 +0100

    Fix init

--- R-code/03-dependent/03-methyl-genexpr-proexpr/init.R
+++ R-code/03-dependent/03-methyl-genexpr-proexpr/init.R
@@ -1,5 +1,5 @@
 source("../init.R", chdir = TRUE)
-source(file.path(function_dir, "methyl-genexpr-proexpr-effect.r"))
+source(file.path(function_dir, "methyl-genexpr-proexpr-effect.R"))
 
 set.seed(4157)
 dep_methyl_genexpr_proexpr_param_data <- methyl_genexpr_proexpr_effect(
