6f4cccf041ab387ea7274114289701ccd072a84e
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-03-12 17:08:30 +0100

    extract and train meta learner only

--- R-code/functions/single-run-rf.R
+++ R-code/functions/single-run-rf.R
@@ -16,8 +16,7 @@ single_run_rf <- function (
                                     sprintf("%s_meta.rds", effect),
                                     collapse = ""))
   training <- readRDS(training_file)
-  # Update meta layer learner with RF.
-  # meta_data <- extractData(object = training)$meta_layer
+  # Update meta layer learner with RF and re-train it.
   meta_layer <- training$getTrainMetaLayer()
   Lrner$new(id = "ranger",
             package = "ranger",
@@ -26,20 +25,8 @@ single_run_rf <- function (
             param_pred_list = list(na_rm = TRUE),
             na_action = "na.rm",
             train_layer = meta_layer)
-  # meta_learner <- meta_layer$getLrnr()
-  # createTrainMetaLayer(training = training,
-  #                      meta_layer_id = "meta_layer",
-  #                      lrner_package = "ranger",
-  #                      lrn_fct = "ranger",
-  #                      param_train_list = list(num.tree = num.tree.meta),
-  #                      param_pred_list = list(na_rm = TRUE),
-  #                      na_action = "na.rm")
-  # Access, update and train the new meta-learner lonly
-  # meta_layer <- training$getTrainMetaLayer()
-  # meta_layer$setTrainData(id = old_meta_layer$getId(),
-  #                       ind_col = training$getIndCol(),
-  #                       data_frame = old_meta_layer$meta_layer)
   meta_layer$train()
+  print(meta_layer)
   # Create testing for predictions
   testing <- createTesting(id = "testing",
                            ind_col = "IDS")
