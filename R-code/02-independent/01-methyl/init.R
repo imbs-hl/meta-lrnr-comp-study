e539ae39270dad6fe172f4cc192ff5f9f6cf088c
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-03-13 10:52:52 +0100

    fixed

--- R-code/02-independent/01-methyl/init.R
+++ R-code/02-independent/01-methyl/init.R
@@ -1,6 +1,6 @@
 source("../init.R", chdir = TRUE)
+print(ddd)
 source(file.path(function_dir, "methyl-effect.R"))
-
 set.seed(4157)
 indep_methy_param_data <- methyl_effect(n = 100L, save_dir = indep_methyl_dir)
 
--- R-code/02-independent/02-methyl-genexpr/init.R
+++ R-code/02-independent/02-methyl-genexpr/init.R
@@ -2,7 +2,7 @@ source("../init.R", chdir = TRUE)
 source(file.path(function_dir, "methyl-genexpr-effect.R"))
 
 set.seed(4157)
-indep_methy_genexpr_param_data <- methyl_genexpr_effect(
+indep_methyl_genexpr_param_data <- methyl_genexpr_effect(
   n = 100L,
   save_dir = indep_methyl_genexpr_dir
 )
