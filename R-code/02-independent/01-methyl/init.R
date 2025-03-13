87dfe9d2318d1adf0de109093cdfc616fbdb3d1c
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-03-13 10:05:37 +0100

    Update source

--- R-code/02-independent/01-methyl/init.R
+++ R-code/02-independent/01-methyl/init.R
@@ -1,5 +1,10 @@
 source("../init.R", chdir = TRUE)
 source(file.path(function_dir, "methyl-effect.R"))
+source(file.path(function_dir, "single-run-best.R"))
+source(file.path(function_dir, "single-run-rf.R"))
+source(file.path(function_dir, "single-run-lasso.R"))
+source(file.path(function_dir, "single-run-lr.R"))
+source(file.path(function_dir, "single-run-cobra.R"))
 
 set.seed(4157)
 indep_methy_param_data <- methyl_effect(n = 100L, save_dir = indep_methyl_dir)
