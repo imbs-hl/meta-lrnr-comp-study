b380cd433db94018201a4872df8b5d39888c0601
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-04-16 15:11:01 +0200

    Renaming

--- R-code/02-independent/02-missing-balanced/01-methyl/init.R
+++ R-code/02-independent/02-missing-balanced/01-methyl/init.R
@@ -5,7 +5,7 @@ indep_missbalanced_me_param_data <- me_effect(
   n = 100L,
   save_dir = indep_missbalanced_me_dir)
 # Rename path to indicate the proportion of missingness
-for(i in 1:length(indep_incombalanced_me_param_data$save_path)) {
+for(i in 1:length(indep_missbalanced_me_param_data$save_path)) {
   indep_missbalanced_me_param_data$save_path[i] <- sub(pattern = "\\.rds$",
                                                             replacement = sprintf("_prop_miss%s.rds", prop_missing_train * 100L),
                                                             x = indep_missbalanced_me_param_data$save_path[i])
