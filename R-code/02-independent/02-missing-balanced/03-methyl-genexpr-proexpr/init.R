68abd5cbfa1b288bb38af863212bc76884000cee
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-04-16 15:05:19 +0200

    renaming

--- R-code/02-independent/02-missing-balanced/03-methyl-genexpr-proexpr/init.R
+++ R-code/02-independent/02-missing-balanced/03-methyl-genexpr-proexpr/init.R
@@ -2,7 +2,7 @@ source("../init.R", chdir = TRUE)
 source(file.path(function_dir, "megepro.R"))
 
 set.seed(4157)
-indep_methyl_genexpr_proexpr_param_data <- methyl_genexpr_proexpr_effect(
+indep_megepro_param_data <- megepro_effect(
   n = 100L,
   save_dir = indep_methyl_genexpr_proexpr_dir
 )
--- R-code/02-independent/02-missing-balanced/init.R
+++ R-code/02-independent/02-missing-balanced/init.R
@@ -24,10 +24,10 @@ dir.create(indep_missbalanced_megepro_dir,
 # ==============================================================================
 #
 reg_indep_missbalanced_me <- file.path(reg_indep_missbalanced_dir, "me")
-reg_indep_missbalanced_megepro <- file.path(reg_independent_dir, "megepro")
-reg_indep_missbalanced_megepro <- file.path(reg_independent_dir, "megepro")
+reg_indep_missbalanced_mege <- file.path(reg_indep_missbalanced_dir, "megepro")
+reg_indep_missbalanced_megepro <- file.path(reg_indep_missbalanced_dir, "megepro")
 dir.create(reg_indep_missbalanced_me, showWarnings = FALSE, recursive = TRUE)
-dir.create(reg_indep_missbalanced_megepro, showWarnings = FALSE, 
+dir.create(reg_indep_missbalanced_mege, showWarnings = FALSE, 
            recursive = TRUE)
 dir.create(reg_indep_missbalanced_megepro, showWarnings = FALSE, 
            recursive = TRUE)
