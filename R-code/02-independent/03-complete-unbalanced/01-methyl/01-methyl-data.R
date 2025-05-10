aeff20dbf9267934001984aca51cc4c1f968a36a
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-05-10 08:49:21 +0200

    Rename file

--- R-code/02-independent/03-complete-unbalanced/01-methyl/01-methyl-data.R
+++ R-code/02-independent/03-complete-unbalanced/01-methyl/01-methyl-data.R
@@ -1,6 +1,6 @@
 source("init.R", chdir = TRUE)
 ## Send jobs
-no.threads <- 5
+no.threads <- 8
 indep_comunbalanced_me_param_data$effect <- NULL
 me_data <- wrap_batchtools(reg_name = "01-data",
                            work_dir = working_dir,
--- R-code/02-independent/03-complete-unbalanced/02-methyl-genexpr/01-methyl-genexpr-data.R
+++ R-code/02-independent/03-complete-unbalanced/02-methyl-genexpr/01-methyl-genexpr-data.R
@@ -1,7 +1,6 @@
 source("init.R", chdir = TRUE)
 ## Send jobs
-no.threads <- 5
-prop_missing_train <- 0.05
+no.threads <- 8
 indep_comunbalanced_mege_param_data$effect <- NULL
 mege_data <- wrap_batchtools(reg_name = "01-data",
                            work_dir = working_dir,
@@ -24,7 +23,7 @@ mege_data <- wrap_batchtools(reg_name = "01-data",
                              function_dir = function_dir
                            ),
                            name = "indep-missunb-mege-data",
-                           overwrite = FALSE,
+                           overwrite = TRUE,
                            memory = "40g",
                            n_cpus = no.threads,
                            walltime = "0",
