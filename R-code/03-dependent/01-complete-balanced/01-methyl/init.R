44201085634318fa8e2fa8601572cdf7d6c185e7
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2025-05-16 13:59:24 +0200

    I retyped them

--- R-code/03-dependent/01-complete-balanced/01-methyl/init.R
+++
@@ -1,7 +0,0 @@
-source("../init.R", chdir = TRUE)
-source(file.path(function_dir, "methyl-effect.R"))
-source(file.path(function_dir, "single-run-best.R"))
-source(file.path(function_dir, "single-run-rf.R"))
-set.seed(4157)
-dep_methy_param_data <- methyl_effect(n = 100L, save_dir = dep_methyl_dir)
-
--- R-code/03-dependent/02-incomplete-balanced/03-methyl-genexpr-proexpr/init.R
+++
@@ -1,8 +0,0 @@
-source("../init.R", chdir = TRUE)
-source(file.path(function_dir, "methyl-genexpr-proexpr-effect.R"))
-
-set.seed(4157)
-dep_methyl_genexpr_proexpr_param_data <- methyl_genexpr_proexpr_effect(
-  n = 100L,
-  save_dir = dep_methyl_genexpr_proexpr_dir
-)
