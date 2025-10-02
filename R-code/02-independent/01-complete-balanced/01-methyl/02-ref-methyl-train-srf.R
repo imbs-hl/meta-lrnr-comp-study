77243f0d55d8375ff41265e712841e410b7c7f2c
devopseasylearning <info@devopseasylearning.com>
2025-10-02 08:35:45 +0200

    Test SRF

--- R-code/02-independent/01-complete-balanced/01-methyl/02-ref-methyl-train-srf.R
+++ R-code/02-independent/01-complete-balanced/01-methyl/02-ref-methyl-train-srf.R
@@ -5,17 +5,17 @@ no.threads <- 5
 ## -----------------------------------------------------------------------------
 ## na_action = na_keep
 ## -----------------------------------------------------------------------------
-reg_me_indep_combalanced_na_keep <- wrap_batchtools(reg_name = "02-train-srf-na-keep",
+reg_me_indep_combalanced_na_keep <- wrap_batchtools(reg_name = "02-train-srf-na-keep-test",
                                                     work_dir = working_dir,
                                                     reg_dir = reg_indep_combalanced_me,
                                                     r_function = single_run_srf,
                                                     vec_args = data.frame(
-                                                      data_file = indep_combalanced_me_param_data$save_path,
-                                                      seed = indep_combalanced_me_param_data$seed,
-                                                      delta.methyl = indep_combalanced_me_param_data$delta.methyl,
-                                                      delta.expr = indep_combalanced_me_param_data$delta.expr,
-                                                      delta.protein = indep_combalanced_me_param_data$delta.protein,
-                                                      effect = indep_combalanced_me_param_data$effect
+                                                      data_file = indep_combalanced_me_param_data[400, ]$save_path,
+                                                      seed = indep_combalanced_me_param_data[400, ]$seed,
+                                                      delta.methyl = indep_combalanced_me_param_data[400, ]$delta.methyl,
+                                                      delta.expr = indep_combalanced_me_param_data[400, ]$delta.expr,
+                                                      delta.protein = indep_combalanced_me_param_data[400, ]$delta.protein,
+                                                      effect = indep_combalanced_me_param_data[400, ]$effect
                                                     ),
                                                     more_args = list(na_action = "na.keep",
                                                                      num.tree.boruta = 25000L,
@@ -24,9 +24,9 @@ reg_me_indep_combalanced_na_keep <- wrap_batchtools(reg_name = "02-train-srf-na-
                                                     overwrite = TRUE,
                                                     memory = "25g",
                                                     n_cpus = 6,
-                                                    walltime = "0",
+                                                    walltime = "60",
                                                     sleep = 5,
-                                                    partition = "prio", ## Set partition in init-global
+                                                    partition = "fast", ## Set partition in init-global
                                                     account = "dzhk-omics", ## Set account in init-global
                                                     test_job = FALSE,
                                                     wait_for_jobs = FALSE,
@@ -47,7 +47,7 @@ reg_me_indep_combalanced_na_keep <- wrap_batchtools(reg_name = "02-train-srf-na-
 ##
 reg_indep_combalanced_me_srf_na_keep <- batchtools::loadRegistry(
   file.dir = file.path(reg_indep_combalanced_me,
-                       "02-train-srf-na-keep"),
+                       "02-train-srf-na-keep-test"),
   writeable = TRUE,
   conf.file = config_file)
 reg_indep_combalanced_me_srf_na_keep <- batchtools::reduceResultsList(
