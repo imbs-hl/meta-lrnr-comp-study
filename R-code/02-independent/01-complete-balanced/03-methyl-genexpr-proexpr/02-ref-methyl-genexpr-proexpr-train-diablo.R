b4ff75f0f9687b1751945e9aefb242fec1356cb4
devopseasylearning <info@devopseasylearning.com>
2025-10-23 10:22:29 +0200

    Fix varsel bug in SRF

--- R-code/02-independent/01-complete-balanced/03-methyl-genexpr-proexpr/02-ref-methyl-genexpr-proexpr-train-diablo.R
+++ R-code/02-independent/01-complete-balanced/03-methyl-genexpr-proexpr/02-ref-methyl-genexpr-proexpr-train-diablo.R
@@ -20,7 +20,7 @@ reg_megepro_indep_combalanced_na_keep <- wrap_batchtools(reg_name = "02-train-di
                                                     ),
                                                     more_args = list(na_action = "na.keep"),
                                                     name = "comb-megepro-diablo-na-keep",
-                                                    overwrite = TRUE,
+                                                    overwrite = FALSE,
                                                     memory = "25g",
                                                     n_cpus = 6,
                                                     walltime = "0",
--- R-code/functions/single-run-srf.R
+++ R-code/functions/single-run-srf.R
@@ -67,13 +67,13 @@ single_run_srf <- function (
   }
   # We train SRF model
   message("Training of SRF model started...\n")
-  srf_trained <- ranger(x = x_training[ , varsel_confirmed, drop = FALSE],
+  srf_trained <- ranger(x = x_training[ , varsel, drop = FALSE],
                         y = as.numeric(y_training == "1"),
                         num.trees = num.tree.ranger,
                         na.action = na_action,
                         probability = TRUE)
   # We predict on the test set
-  predictions <- predict(object = srf_trained,
+  predictions <- predict(object = srf_trained[ , varsel, drop = FALSE],
                          data = x_testing)
   end_time <- Sys.time()  # Record end time
   pred_values <- data.frame(test_ids, predictions$predictions[ , 2L])
